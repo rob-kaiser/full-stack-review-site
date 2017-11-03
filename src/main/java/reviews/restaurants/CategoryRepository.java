69865d663da88a3478720eba94465b76becf181d
Rob Kaiser <rob.kaiser22@gmail.com>
2017-11-02 20:49:10 -0400

    updated again

--- src/main/java/reviews/restaurants/CategoryRepository.java
+++ src/main/java/reviews/restaurants/CategoryRepository.java
@@ -1,11 +1,8 @@
 package reviews.restaurants;
 
-
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
-​ 
-@Component
+import org.springframework.ui.Model;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
-	
-​
+
 }
